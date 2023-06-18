2546e4c717e76a84d38aa7dcacac3ec3f34025a3
beesou777 <sbeesou@gmail.com>
2023-06-18 07:43:46 +0545

    raw

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const accessToken = JSON.parse(localStorage.getItem('access_token')) || null;
-// axios.defaults.baseURL = 'https://coupoun-backend.up.railway.app/';
-axios.defaults.baseURL = 'http://localhost:8000/';
+axios.defaults.baseURL = 'https://coupoun-backend.up.railway.app/';
+// axios.defaults.baseURL = 'http://localhost:8000/';
 // 
 export default axios;
